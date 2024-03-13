b5d81059034043f967c01d15c81e461444a32fd1
Bruno Landim Toledo <88839468+brult32@users.noreply.github.com>
2024-03-13 14:56:18 -0300

    Add files via upload

---
+++ atividade13.03.js
@@ -0,0 +1,54 @@
+class Student {
+	constructor(name, age, birthyear, job, seclang) {
+		this.name = name;
+		this.age = age;
+		this.birthyear = birthyear;
+		this.job = job;
+		this.seclang = seclang;
+	}
+}
+
+const myNames = ['Adriano', 'Bruno', 'Danilo', 'Jasa', 'Julia', 'Kevin', 'Mari', 'Paulino', 'Schiffel'];
+const myAges = ['25', '39', '17', '17', '17', '17', '17', '32', '17'];
+const myJob = ['Contrução Civil', 'Professor', 'Estudante', 'Estudante', 'Estudante', 'Estudante', 'Estudante', 'Marido Troféu', 'Estudante'];
+const mySecLang = ['Espanhol', 'Inglês', 'Russo', 'Japones', 'Francês', 'Espanhol', 'Koreano', 'Inglês', 'Alemão'];
+
+let objArray = [];
+
+function getBirthYear(age){
+	let thisYear = new Date().getFullYear();
+	// console.log(thisYear - myAges[index]);
+	return thisYear - age;
+}
+function populateObjArray(){
+	for (let index in myNames){
+		objArray.push(new Student(myNames[index], myAges[index], getBirthYear(myAges[index]), myJob[index], mySecLang[index]))
+		// console.log(objArray[index].name);
+	}
+}
+populateObjArray();
+
+function checkIfStudent(job){
+	if (job == 'Estudante'){
+		return `é estudante\ndo E.M.`;
+	} else {
+		return `tem formação\nanterior como ${job} e`;
+	}
+}
+function printCurriculum(obj){
+	console.log(`${obj.name}, ${obj.age} anos, estudante de informática\npara internet, nasceu no ano ${obj.birthyear}, ${checkIfStudent(obj.job)}, e tem ${obj.seclang} como segundo idioma.\n`);
+}
+function printAll(){
+	for(let person of objArray){
+		printCurriculum(person);
+	}
+}
+printAll();
+
+// let mySorted = myNames.sort();
+// console.log(mySorted);
+
+/*
+para esta atividade não vi dificuldades, apenas na hora de usar loop 'for in'-'for of', 
+normalmente tenho que testar para lembrar qual busca pelo indice, qual pelo valor.
+*/
